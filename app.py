f1979d07eadef9bd95d97bc104aaf6663b71f738
Mihir Bindal <59363084+MihirBindal@users.noreply.github.com>
2020-07-10 20:05:46 +0530

    updated the frontend

--- app.py
+++ app.py
@@ -1,49 +1,27 @@
 from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
 import glob
 import numpy as np
 from keras.preprocessing import image
-
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-
 from keras.models import load_model
 from keras import backend
 from tensorflow.keras import backend
-
 import tensorflow as tf
-
 global graph
 graph = tf.get_default_graph()
-
-# global graph
-# graph = tf.get_default_graph()
-
-
 from skimage.transform import resize
 
-# Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
-# Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/crop_protection.h5'
+MODEL_PATH = 'mymodel.h5'
 
-# Load your trained model
 model = load_model(MODEL_PATH)
-# Necessary
-# print('Model loaded. Start serving...')
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-# from keras.applications.resnet50 import ResNet50
-# model = ResNet50(weights='imagenet')
-# model.save('')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
@@ -70,8 +48,8 @@ def upload():
 
         with graph.as_default():
             preds = model.predict_classes(x)
-        index = ['Bears', 'Crows', 'Elephants', 'Racoons', 'Rats']
-        text = "prediction :  " + index[preds[0]]
+        index = ['Normal', 'Pneumoniac']
+        text = index[preds[0][0]]
 
         # ImageNet Decode
 
